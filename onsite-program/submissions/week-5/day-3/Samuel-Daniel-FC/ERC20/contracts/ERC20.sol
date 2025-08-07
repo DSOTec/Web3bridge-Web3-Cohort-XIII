ebb94d22be04d5d5e61ea27dfceb7f1a6dfbe7f0
DSOTec <sdamiloladaniel@gmail.com>
2025-08-07 01:11:45 +0100

    ERC20Fctory

---
+++ onsite-program/submissions/week-5/day-3/Samuel-Daniel-FC/ERC20/contracts/ERC20.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import "./IERC20.sol";
+
+contract ERC20 is IERC20 {
+    string public name = "MyToken";
+    string public symbol = "MTK";
+    uint8 public decimals = 18;
+
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    function transfer(address recipient, uint256 amount) external override returns (bool) {
+        require(balanceOf[msg.sender] >= amount, "ERC20: transfer amount exceeds balance");
+        balanceOf[msg.sender] -= amount;
+        balanceOf[recipient] += amount;
+        emit Transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) external override returns (bool) {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) external override returns (bool) {
+        require(allowance[sender][msg.sender] >= amount, "ERC20: transfer amount exceeds allowance");
+        require(balanceOf[sender] >= amount, "ERC20: transfer amount exceeds balance");
+        allowance[sender][msg.sender] -= amount;
+        balanceOf[sender] -= amount;
+        balanceOf[recipient] += amount;
+        emit Transfer(sender, recipient, amount);
+        return true;
+    }
+
+    function _mint(address to, uint256 amount) internal {
+        require(to != address(0), "ERC20: mint to the zero address");
+        totalSupply += amount;
+        balanceOf[to] += amount;
+        emit Transfer(address(0), to, amount);
+    }
+
+    function _burn(address from, uint256 amount) internal {
+        require(from != address(0), "ERC20: burn from the zero address");
+        require(balanceOf[from] >= amount, "ERC20: burn amount exceeds balance");
+        balanceOf[from] -= amount;
+        totalSupply -= amount;
+        emit Transfer(from, address(0), amount);
+    }
+
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) external {
+        _burn(from, amount);
+    }
+}
